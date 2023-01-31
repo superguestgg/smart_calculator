ae4ca34a136e2aedba0a567b298312412038fdbb
Ilya <82775046+superguestgg@users.noreply.github.com>
2023-01-31 22:46:40 +0500

    Add files via upload

--- start2.py
+++ start2.py
@@ -19,16 +19,16 @@ else:
     training_data, test_data = get_data_2.get_data(operation, ranges)
 
 
-    net = network.Network([40, 20, 30])
+    net = network.Network([40, 30, 30])
     net.SGD(training_data, 10, 20, 5.0, evaluation_data=test_data, lmbda=0.0,
         monitor_evaluation_accuracy=False, monitor_evaluation_cost=False,
         monitor_training_cost=True, monitor_training_accuracy=True)
     while True:
         a, b = map(int, input().split())
         result = net.feedforward(get_data_2.get_arrays_by_numbers(a, b, ranges))
-        result_str=""
+        result_str = ""
         for i in range (ranges.get_ranges_count()[2]):
-            if max(result[i*10:(i+1)*10])>0.01:
+            if max(result[i*10:(i+1)*10]) > 0.5:
                 result_str = str(np.argmax(result[i*10:(i+1)*10])) + result_str
             else:
                 result_str = "0" + result_str
