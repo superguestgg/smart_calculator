55f4bd0f8e4678812944c6548e69042e943d8ec6
Ilya <82775046+superguestgg@users.noreply.github.com>
2023-02-03 14:11:27 +0500

    small refactoring

--- draw.py
+++ draw.py
@@ -4,16 +4,16 @@ import sys
 def draw_by_pixels(np_array, width=28):
     result = "\n"
     left = 0
-    thisline = ""
+    this_line = ""
     for pixel in np_array:
         pixel = pixel[0]
         left += 1
-        thisline += "#"*(pixel > 0.66)+" "*(pixel < 0.33)\
-                    + "="*(0.66 >= pixel >= 0.33)
+        this_line += "#" * (pixel > 0.66) + " " * (pixel < 0.33) \
+                     + "=" * (0.66 >= pixel >= 0.33)
         if left >= width:
             left = 0
-            result += thisline + "\n"
-            thisline = ""
+            result += this_line + "\n"
+            this_line = ""
 
     sys.stdout.write(result)
     return result
